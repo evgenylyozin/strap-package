7e28bb0e6b7bd3c18439cd48e378544e71fd61d6
Evgeny Lyozin <evgenylyozindev@gmail.com>
2025-01-21 19:00:58 +0300

    add additional questions and info

--- src/index.ts
+++ src/index.ts
@@ -29,165 +29,271 @@ HeaderLog(
 
 const asyncLookup = promisify(lookup);
 (async () => {
-  // first goes the online check
-  const checkIfOnline = async () => {
-    try {
-      InfoLog("Checking if you are online...");
-      await asyncLookup("registry.npmjs.org");
-      SuccessLog("You are online");
-    } catch {
+  try {
+    // first goes the online check
+    const checkIfOnline = async () => {
+      try {
+        InfoLog("Checking if you are online...");
+        await asyncLookup("registry.npmjs.org");
+        SuccessLog("You are online");
+      } catch {
+        ErrorLog(
+          "You appear to be offline. Please connect to use strap-package...",
+        );
+        process.exit(1);
+      }
+    };
+    await checkIfOnline();
+    // then check that current node is LTS
+    const currentNodeVersion = process.versions.node;
+    const LTSNodeVersion = process.release.sourceUrl
+      .split("node-v")[1]
+      .replace(".tar.gz", "");
+    InfoLog("Checking Node version to be LTS...");
+    if (currentNodeVersion !== LTSNodeVersion) {
+      // if not LTS then ask to use LTS node
       ErrorLog(
-        "You appear to be offline. Please connect to use strap-package...",
+        "You are running Node " +
+          currentNodeVersion +
+          "\n" +
+          "Strap package requires LTS Node to be used. \n" +
+          "Currently the LTS version is " +
+          LTSNodeVersion +
+          "\nPlease update your version of Node.",
       );
       process.exit(1);
     }
-  };
-  await checkIfOnline();
-  // then check that current node is LTS
-  const currentNodeVersion = process.versions.node;
-  const LTSNodeVersion = process.release.sourceUrl
-    .split("node-v")[1]
-    .replace(".tar.gz", "");
-  InfoLog("Checking Node version to be LTS...");
-  if (currentNodeVersion !== LTSNodeVersion) {
-    // if not LTS then ask to use LTS node
-    ErrorLog(
-      "You are running Node " +
-        currentNodeVersion +
-        "\n" +
-        "Strap package requires LTS Node to be used. \n" +
-        "Currently the LTS version is " +
-        LTSNodeVersion +
-        "\nPlease update your version of Node.",
-    );
-    process.exit(1);
-  }
-  SuccessLog("You are running Node " + currentNodeVersion + " which is LTS");
-  // gathering info
-  const Settings = {
-    name: "default-package",
-    path: process.cwd(),
-    target: "both",
-    packageManager: "npm",
-    buildTool: "webpack",
-    language: "typescript",
-    testRunner: "vitest",
-    linter: true,
-    formatter: true,
-    shouldInitializeGit: true,
-    shouldIncludeMITLicense: true,
-  };
-  const FormattedSettings = () => {
-    return JSON.stringify(Settings, null, 2);
-  };
-  InfoLog("Before naming the package:");
-  const shouldCheckPackageNameAvailability = await confirm({
-    message:
-      "Do you want to check if the package name is available on npm and is valid?",
-    default: true,
-  });
-  if (!shouldCheckPackageNameAvailability) {
-    WarningLog(
-      "Skipping package name availability check and package name validation...",
-    );
-    WarningLog(
-      "Be ware that the package name might not be valid or could already be taken...",
-    );
-  }
-  const Naming = async (shouldCheck: boolean) => {
-    const name = await input({
-      message: "What is the desired name of the package?",
-      default: "default-package",
+    SuccessLog("You are running Node " + currentNodeVersion + " which is LTS");
+    const UnchangeableDefaults = {
+      path: process.cwd(),
+      packageManager: "npm",
+      buildTool: "webpack",
+      language: "typescript",
+      testRunner: "vitest",
+      linter: "eslint",
+      formatter: "prettier",
+    };
+    // gathering info
+    const Settings = {
+      name: "default-package",
+      target: "both",
+      shouldInitializeGit: true,
+      shouldIncludeMITLicense: true,
+      shouldIncludeHuskyPrecommit: true,
+      shouldIncludeNPMPublishWorkflow: true,
+      shouldIncludeReadme: true,
+      shouldIncludeDevelopmentReadme: true,
+      shouldIncludeGenericIgnoreFiles: true,
+    };
+    const FormattedSettings = (
+      settingsObject: typeof Settings | typeof UnchangeableDefaults,
+    ) => {
+      return JSON.stringify(settingsObject, null, 2);
+    };
+    InfoLog("Before naming the package:");
+    const shouldCheckPackageNameAvailability = await confirm({
+      message:
+        "Do you want to check if the package name is available on npm and is valid?",
+      default: true,
     });
-    if (shouldCheck) {
-      InfoLog("Checking if the package name is valid...");
-      const { validForNewPackages, errors, warnings } = validate(name); // check for name validity
-      if (!validForNewPackages) {
-        ErrorLog("Invalid package name for new package!");
-        if (errors) {
-          ErrorLog("Errors:" + errors);
-        }
-        if (warnings) {
-          WarningLog("Warnings:" + warnings);
-        }
-        InfoLog("Try another name...");
-        return await Naming(shouldCheck);
-      }
-      SuccessLog("Selected package name is valid");
-      // check for name availability
-      InfoLog("Checking if the package name is available...");
-      if (
-        (await (await fetch(`http://registry.npmjs.org/${name}`)).json()).name
-      ) {
-        ErrorLog("Package name already taken!");
-        InfoLog(`See https://www.npmjs.com/package/${name}`);
-        InfoLog("Try another name...");
-        return await Naming(shouldCheck);
-      }
-      SuccessLog("Selected package name is available");
+    if (!shouldCheckPackageNameAvailability) {
+      WarningLog(
+        "Skipping package name availability check and package name validation...",
+      );
+      WarningLog(
+        "Be ware that the package name might not be valid or could already be taken...",
+      );
     }
-    // all is fine, set the selected name to the Settings object
-    Settings.name = name;
-  };
+    const Naming = async (shouldCheck: boolean) => {
+      const name = await input({
+        message: "What is the desired name of the package?",
+        default: "default-package",
+      });
+      if (shouldCheck) {
+        InfoLog("Checking if the package name is valid...");
+        const { validForNewPackages, errors, warnings } = validate(name); // check for name validity
+        if (!validForNewPackages) {
+          ErrorLog("Invalid package name for new package!");
+          if (errors) {
+            ErrorLog("Errors:" + errors);
+          }
+          if (warnings) {
+            WarningLog("Warnings:" + warnings);
+          }
+          InfoLog("Try another name...");
+          return await Naming(shouldCheck);
+        }
+        SuccessLog("Selected package name is valid");
+        // check for name availability
+        InfoLog("Checking if the package name is available...");
+        if (
+          (await (await fetch(`http://registry.npmjs.org/${name}`)).json()).name
+        ) {
+          ErrorLog("Package name already taken!");
+          InfoLog(`See https://www.npmjs.com/package/${name}`);
+          InfoLog("Try another name...");
+          return await Naming(shouldCheck);
+        }
+        SuccessLog("Selected package name is available");
+      }
+      // all is fine, set the selected name to the Settings object
+      Settings.name = name;
+    };
 
-  await Naming(shouldCheckPackageNameAvailability);
+    await Naming(shouldCheckPackageNameAvailability);
 
-  InfoLog("The package could be using APIs which are available only in node");
-  InfoLog("Like crypto, fs, path, os, etc.");
-  InfoLog("Or APIs which are available only in browser");
-  InfoLog("Like window object etc.");
-  InfoLog("Or it could be hybrid, using APIs from both");
-  InfoLog(
-    "Selecting which APIs will be used affect dependencies, especially the type and build tools",
-  );
-  Settings.target = await select<"node" | "browser" | "both">({
-    message: "Which APIs will the package be using?",
-    choices: ["node", "browser", "both"],
-    default: "both",
-  });
+    // use defaults or select specific settings
+    HeaderLog("Choosing settings");
+    WarningLog("Notice! The following settings are not customizable:");
+    InfoLog(FormattedSettings(UnchangeableDefaults));
+    WarningLog("The project will always have these tools set up");
+    WarningLog(
+      "and the current working directory as a starting point to generate the boilerplate",
+    );
+    const confirmToUseUnchangeableDefaults = await confirm({
+      message: "Do you agree to use this setup?",
+      default: true,
+    });
+    if (!confirmToUseUnchangeableDefaults) {
+      SuccessLog(
+        "There are many tools to start an npm package project with different setup",
+      );
+      InfoLog(
+        "See for example: https://www.npmjs.com/search?q=npm%20package%20boilerplate",
+      );
+      InfoLog("Exiting strap-package...");
+      process.exit(1);
+    }
+    SuccessLog("Default setup is approved. Choose the customizable settings.");
+    InfoLog("Default settings are:");
+    InfoLog(FormattedSettings(Settings));
+    const shouldUseDefaults = await confirm({
+      message: "Do you want to use default settings?",
+      default: true,
+    });
+    if (!shouldUseDefaults) {
+      InfoLog("The package could be targeting Node, Browser or both");
+      InfoLog(
+        "Selecting specific platform allows for using specific platform APIs",
+      );
+      InfoLog("Like crypto, fs, path, os, etc. in Node");
+      InfoLog("Or window object etc. in Browser");
+      InfoLog(
+        "If the package is intended to be used in both environments, then choose 'both'",
+      );
+      WarningLog(
+        "In this case the used APIs should be available both in Node and Browser",
+      );
+      WarningLog(
+        "So be careful and check if the used APIs are available in both environments",
+      );
+      Settings.target = await select<"node" | "browser" | "both">({
+        message: "Choose the target for the package",
+        choices: ["node", "browser", "both"],
+        default: "both",
+      });
 
-  InfoLog("Initializing the git repository is done with just 'git init'");
-  Settings.shouldInitializeGit = await confirm({
-    message: "Do you want to initialize a git repository?",
-    default: true,
-  });
-  InfoLog(
-    "MIT license allows others to use/modify etc. your package free of charge",
-  );
-  InfoLog("See full license at https://choosealicense.com/licenses/mit/");
-  Settings.shouldIncludeMITLicense = await confirm({
-    message: "Do you want to include MIT license?",
-    default: true,
-  });
-  SuccessLog("Selected settings:\n" + FormattedSettings());
-  InfoLog("Initializing the package in the current directory...");
-  // executing commands to install all the dependencies
-  // and setup the package skeleton
-  // should use single exec command after all the data has been collected
-  // else the nvm is not available in other shell or node version is not the needed one
-  //   try {
-  //     console.log("Installing NVM...");
-  //     await asyncExec(nvmInstallString);
-  //     const enableNVMString = `
-  //     unset npm_config_prefix # required for nvm install if fnm is installed
-  //     export NVM_DIR="$HOME/.nvm"
-  //     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-  //     [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  // `;
-  //     const installLTSNodeString = `nvm install --lts`;
-  //     console.log("Enabling NVM and installing LTS node...");
-  //     const { stdout } = await asyncExec(
-  //       enableNVMString + " " + installLTSNodeString,
-  //     );
-  //     console.log(stdout);
-  //     console.log("Installing LTS node... done");
-  //     console.log(
-  //       "Current node version: " +
-  //         JSON.stringify(await asyncExec("node --version")),
-  //     );
-  //   } catch (error) {
-  //     console.error("Failed to install NVM...");
-  //     console.error(error);
-  //     process.exit(1);
-  //   }
+      InfoLog("Initializing the git repository is done with just 'git init'");
+      Settings.shouldInitializeGit = await confirm({
+        message: "Do you want to initialize a git repository?",
+        default: true,
+      });
+      InfoLog(
+        "MIT license allows others to use/modify etc. your package free of charge",
+      );
+      InfoLog("See full license at https://choosealicense.com/licenses/mit/");
+      Settings.shouldIncludeMITLicense = await confirm({
+        message: "Do you want to include MIT license?",
+        default: true,
+      });
+
+      InfoLog(
+        "Including husky pre-commit hook is usually useful to test, lint and format the code before commit is made",
+      );
+      Settings.shouldIncludeHuskyPrecommit = await confirm({
+        message: "Do you want to include husky pre-commit hook?",
+        default: true,
+      });
+
+      InfoLog("Including npm publish workflow for publishing to npm registry");
+      InfoLog(
+        "Adds a simple workflow which is used by github actions to publish the package to npm registry",
+      );
+      InfoLog(
+        "Additional info is going to be generated in the README.dev.md file if the selected settings allow for this file to be included",
+      );
+      Settings.shouldIncludeNPMPublishWorkflow = await confirm({
+        message: "Do you want to include npm publish workflow?",
+        default: true,
+      });
+
+      InfoLog(
+        "Including README.md file will add this file along with some generic sections",
+      );
+      InfoLog(
+        "Such file usually contains information about the package like how to install and use it etc.",
+      );
+      Settings.shouldIncludeReadme = await confirm({
+        message: "Do you want to include README.md file?",
+        default: true,
+      });
+
+      InfoLog(
+        "Including DEV.README.md file will add this file along with some generic sections",
+      );
+      InfoLog(
+        "Such file usually contains development information like how to build, test, run etc.",
+      );
+      Settings.shouldIncludeDevelopmentReadme = await confirm({
+        message: "Do you want to include development README.md file?",
+        default: true,
+      });
+      InfoLog(
+        "Including generic ignore files will add files like .gitignore and .npmignore",
+      );
+      InfoLog(
+        "Such files usually contain information about files to be ignored by git and npm etc.",
+      );
+      Settings.shouldIncludeGenericIgnoreFiles = await confirm({
+        message: "Do you want to include generic ignore files?",
+        default: true,
+      });
+    }
+    SuccessLog(
+      "Selected settings:\n" +
+        FormattedSettings({ ...UnchangeableDefaults, ...Settings }),
+    );
+    InfoLog("Initializing the package in the current directory...");
+    // executing commands to install all the dependencies
+    // and setup the package skeleton
+    // should use single exec command after all the data has been collected
+    // else the nvm is not available in other shell or node version is not the needed one
+    //   try {
+    //     console.log("Installing NVM...");
+    //     await asyncExec(nvmInstallString);
+    //     const enableNVMString = `
+    //     unset npm_config_prefix # required for nvm install if fnm is installed
+    //     export NVM_DIR="$HOME/.nvm"
+    //     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    //     [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    // `;
+    //     const installLTSNodeString = `nvm install --lts`;
+    //     console.log("Enabling NVM and installing LTS node...");
+    //     const { stdout } = await asyncExec(
+    //       enableNVMString + " " + installLTSNodeString,
+    //     );
+    //     console.log(stdout);
+    //     console.log("Installing LTS node... done");
+    //     console.log(
+    //       "Current node version: " +
+    //         JSON.stringify(await asyncExec("node --version")),
+    //     );
+    //   } catch (error) {
+    //     console.error("Failed to install NVM...");
+    //     console.error(error);
+    //     process.exit(1);
+    //   }
+  } catch {
+    // the error handling from CTRL+C
+  }
 })();
